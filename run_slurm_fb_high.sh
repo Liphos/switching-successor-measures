b0a28b42f3c25e854d26fb59e7b52f515450d7a6
Guillaume Damien Nicolas Levy <glevy@meta.com>
2026-06-10 21:10:53 +0200

    run: run high policy on stable fb

--- run_slurm_fb_high.sh
+++ run_slurm_fb_high.sh
@@ -14,4 +14,4 @@ SEED=${SEEDS[$SLURM_ARRAY_TASK_ID]}
 # Activate the project's local venv.
 source .venv/bin/activate
 
-python main.py --env_name=ogbench-antmaze-medium-navigate-v0 --agent=agents/fbpiswitch.py --train_steps=500_000 --agent.frozen_path="/checkpoint/glevy/fb_hier/sd3502_s_64119910.0.20260604_163929" --agent.high_alpha=1e-3  --seed="$SEED"
+python main.py --env_name=ogbench-antmaze-medium-navigate-v0 --agent=agents/fb_hierarchical.py --train_steps=500_000 --agent.frozen_path="/checkpoint/glevy/fb_hier/sd9460_s_64427509.0.20260610_141643" --agent.high_alpha=1e-3  --seed="$SEED"
