f18e345a19b296337e2df0c9cbb5988f1d8bca8d
Guillaume Damien Nicolas Levy <glevy@meta.com>
2026-06-10 14:53:41 +0200

    run: remove init

--- run_slurm.sh
+++ run_slurm.sh
@@ -27,5 +27,5 @@ source .venv/bin/activate
 srun python main.py \
     --env_name=ogbench-antmaze-${ENV}-navigate-v0 \
     --agent=agents/fb.py \
-    --wandb_run_group=fb_stable_relu_splitemb_ \
+    --wandb_run_group=fb_stable_relu_splitemb_rminit \
     --seed="$SEED"
