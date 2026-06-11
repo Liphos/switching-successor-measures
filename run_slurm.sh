7dedc508ed7e0f664197f29bf4a04986fd2a9513
glevy <levyguillaume10@yahoo.fr>
2026-06-11 14:53:49 +0000

    run: offdiag with cov

--- run_slurm.sh
+++ run_slurm.sh
@@ -27,5 +27,5 @@ source .venv/bin/activate
 srun python main.py \
     --env_name=ogbench-antmaze-${ENV}-navigate-v0 \
     --agent=agents/fb.py \
-    --wandb_run_group=fb_offdiag \
+    --wandb_run_group=fb_offdiag_with_cov \
     --seed="$SEED"
